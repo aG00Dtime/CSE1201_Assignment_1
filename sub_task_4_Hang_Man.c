b6dd9f0e6b5aa1c121d73375ff664f6b38e73133
aG00Dtime <77511476+aG00Dtime@users.noreply.github.com>
2021-05-21 10:40:54 -0400

    Clean up

--- sub_task_4_Hang_Man.c
+++ sub_task_4_Hang_Man.c
@@ -6,12 +6,13 @@
 #include <conio.h>
 
 // prototypes
-int word_selector();
-int print_hangman(int x);
+int word_selector();	//random number to select a random word
+int print_hangman(int);	// prints hangman image
+int word_print(int, int);	//prints word with blanks and hangman image
 
-// variables
-char words[][10] = {"dog",   "cow",   "computer", "onion",   "house",
-                    "apple", "pizza", "soda",     "chicken", "potato"};
+// variables 
+char words[][10] = { "dog", "cow", "computer", "onion", "house",
+	"apple", "pizza", "soda", "chicken", "potato" };
 char letter[10];
 
 int i, word_index, random_number;
@@ -22,194 +23,213 @@ int hangman = 0;
 int playing = 1;
 
 // main
-int main() {
-  // clear screen at start
-  system("cls");
+int main()
+{
+	// clear screen at start
+	system("cls");
 
-  // value is generated to choose the word
-  word_index = word_selector();
-  int word_length = strlen(words[word_index]);
+	// value is generated to choose the word
+	word_index = word_selector();
+	int word_length = strlen(words[word_index]);	//length of the word
 
-  // intro screen with word list
-  print_hangman(10);
-  printf(" Press any key to begin...");
-  getch();
-  system("cls");
+	// intro screen with word list
+	print_hangman(10);
+	printf("\nPress any key to begin...");
+	getch();
+	system("cls");
 
-  // print the initial screen with the blanks
-  print_hangman(0);
-  for (i = 0; i < word_length; i++) {
-    if (!word_hidden[i]) {
-      printf(" _ ");
-    }
-  }
+	// print the initial screen with the blanks
+	word_print(hangman, word_length);
 
-  // game loop
-  while (playing) {
+	// game loop
+	while (playing)
+	{
+		// check to see if guesses are used up or player won
+		if (correct == word_length)
+		{
+			printf("\n\n YOU WIN!\nPress any key to exit..\n");
+			getch();
+			playing = 0;
+			continue;
+		}
 
-    // check to see if guesses are used up or player won
-    if (correct == word_length) {
-      printf("\n\n YOU WIN!\n\n");
-      printf(" Press any key to exit...");
-      getch();
-      playing = 0;
-      continue;
-    }
+		if (guessed == 5)
+		{
+			printf("\n\n You lose!\n\n");
+			printf("\n The word was %s ! \n", words[word_index]);
+			printf(" Press any key to exit...");
+			getch();
+			playing = 0;
+			continue;
+		}
 
-    if (guessed == 5) {
-      printf("\n\n You lose!\n\n");
-      printf("\n The word was %s ! \n",words[word_index]);
-      printf(" Press any key to exit...");
-      getch();
-      playing = 0;
-      continue;
-    }
+		// ask player to guess
+		printf("\n\n Remaining guesses : %d \n\n Enter a letter:", 5 - guessed);
+		fgets(letter, 10, stdin);
 
-    // ask player to guess
-    printf("\n\n Remaining guesses : %d", 5 - guessed);
-    printf("\n\n Enter a letter:");
-    fgets(letter,10,stdin);
-    //scanf(" %c", letter);
+		// current "correct" value count is stored,a change indicates guess was correct
+		guess_check = correct;
 
-    // current "correct" value count is stored,a change indicates guess was correct
-      guess_check = correct;
+		// loop through letters to see if it matches player guess,skip ones that are already guessed
+		for (i = 0; i < word_length; i++)
+		{
+			if (word_hidden[i] == 1)
+			{
+				continue;
+			}
 
-    // loop through letters to see if it matches player guess,skip ones that are already guessed
-    for (i = 0; i < word_length; i++) {
-      if (word_hidden[i] == 1) {
-        continue;
-      }
-      // increase correct count if letter exists in the word
-      if (letter[0] == words[word_index][i]) {
-        word_hidden[i] = 1;
-        correct++;
-      }
-    }
+			// increase correct count if letter exists in the word
+			if (letter[0] == words[word_index][i])
+			{
+				word_hidden[i] = 1;
+				correct++;
+			}
+		}
 
-    // Clear the screen after guessing
-    system("cls");
+		// Clear the screen after guessing
+		system("cls");
 
-    // check if guess was incorrect
-    if (correct > guess_check) {
-      printf("\n Correct guess!");
-    } else {
-      printf("\n Incorrect Guess!");
-      guessed++;
-      hangman++;
-    }
+		// check if guess was incorrect
+		if (correct > guess_check)
+		{
+			printf("\n Correct guess!");
+		}
+		else
+		{
+			printf("\n Incorrect Guess!");
+			guessed++;
+			hangman++;
+		}
 
-    // print new hangman image and correctly guessed letters
-    print_hangman(hangman);
-    for (i = 0; i < word_length; i++) { 
+		// print new hangman image and correctly guessed letters    
+		word_print(hangman, word_length);
+	}
 
-      if (word_hidden[i]) {
-        printf(" %c ", words[word_index][i]);
-      } else {
-        printf(" _ ");
-      }
-    }
-  }
+	return 0;
+}
 
-  return 0;
+// function that prints the word / blanks and calls the hangman image func
+int word_print(int hangman, int word_length)
+{
+	print_hangman(hangman);
+
+	for (i = 0; i < word_length; i++)
+	{
+		if (word_hidden[i])
+		{
+			printf(" %c ", words[word_index][i]);
+		}
+		else
+		{
+			printf(" _ ");
+		}
+	}
 }
 
 // function to print hanged man
-int print_hangman(int x) {
-
-  if (x == 0) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  } 
-  else if (x == 1) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |    0");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  } 
-  else if (x == 2) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |    0");
-    printf("\n |   /|");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  } 
-  else if (x == 3) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |    0");
-    printf("\n |   /|\\");
-    printf("\n |");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  }
-  else if (x == 4) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |    0");
-    printf("\n |   /|\\");
-    printf("\n |   / ");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  }
-  else if (x == 5) {
-    printf("\n");
-    printf("\n +----+");
-    printf("\n |    |");
-    printf("\n |    0");
-    printf("\n |   /|\\");
-    printf("\n |   / \\");
-    printf("\n |");
-    printf("\n |");
-    printf("\n +----+\n\n");
-  }
-  else if (x == 10) {
-    printf("\n *** HANG MAN "
-           "******************************************************");
-    printf("\n      +----+");
-    printf("\n      |    |");
-    printf("\n      |    0");
-    printf("\n      |   /|\\");
-    printf("\n      |   / \\");
-    printf("\n      |");
-    printf("\n      |");
-    printf("\n      +----+\n");
-    printf("\n "
-           "*******************************************************************"
-           "*  \n");
-    printf("\n Words: "
-           "dog,cow,computer,onion,house,apple,pizza,soda,chicken,potato\n");
-    printf("\n "
-           "*******************************************************************"
-           "*  \n");
-  }
+int print_hangman(int x)
+{
+	if (x == 0)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 1)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |    0");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 2)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |    0");
+		printf("\n |   /|");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 3)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |    0");
+		printf("\n |   /|\\");
+		printf("\n |");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 4)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |    0");
+		printf("\n |   /|\\");
+		printf("\n |   / ");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 5)
+	{
+		printf("\n");
+		printf("\n +----+");
+		printf("\n |    |");
+		printf("\n |    0");
+		printf("\n |   /|\\");
+		printf("\n |   / \\");
+		printf("\n |");
+		printf("\n |");
+		printf("\n +----+\n\n");
+	}
+	else if (x == 10)
+	{
+		printf("\n ***HANG MAN "
+			"******************************************************");
+		printf("\n      +----+");
+		printf("\n      |    |");
+		printf("\n      |    0");
+		printf("\n      |   /|\\");
+		printf("\n      |   / \\");
+		printf("\n      |");
+		printf("\n      |");
+		printf("\n      +----+\n");
+		printf("\n "
+			"*******************************************************************"
+			"* \n");
+		printf("\n Words: "
+			"dog,cow,computer,onion,house,apple,pizza,soda,chicken,potato\n");
+		printf("\n "
+			"*******************************************************************"
+			"* \n");
+	}
 }
 
 // generate a number between 0 and 10 - uses as the array index
-int word_selector() {
+int word_selector()
+{
+	srand((unsigned) time(NULL));
 
-  srand((unsigned)time(NULL));
+	random_number = rand() % 10;
 
-  random_number = rand() % 10;
-
-  return random_number;
+	return random_number;
 }
